1de22d12170cc3ede616dbc2f82b0dbbbfa97e45
salmaahesham99 <85070792+salmaahesham99@users.noreply.github.com>
2021-05-30 18:11:05 +0200

    Create main.php

---
+++ phpmail/main.php
@@ -0,0 +1,33 @@
+<?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+    require 'PHPMailer-master/src/Exception.php';
+    require 'PHPMailer-master/src/PHPMailer.php';
+    require 'PHPMailer-master/src/SMTP.php';
+
+    $mail = new PHPMailer();
+    $mail->IsSMTP();
+    $mail->Mailer = "smtp";
+
+    $mail->SMTPDebug  = 1;  
+    $mail->SMTPAuth   = TRUE;
+    $mail->SMTPSecure = "tls";
+    $mail->Port       = 587;
+    $mail->Host       = "smtp.gmail.com";
+    $mail->Username   = "GoFootballSystem@gmail.com";
+    $mail->Password   = "GoFootballSystem11";
+
+    $mail->IsHTML(true);
+    $mail->AddAddress("ahmed.naeem2027@gmail.com", "mr naeem");
+    $mail->SetFrom("GoFootballSystem@gmail.com", "from-name");
+    $mail->Subject = "Test is Test Email sent via Gmail SMTP Server using PHP Mailer";
+    $content = "<b>This is a Test Email sent via Gmail SMTP Server using PHP mailer class.</b>";
+
+    $mail->MsgHTML($content); 
+    if(!$mail->Send()) {
+    echo "Error while sending Email.";
+    var_dump($mail);
+    } else {
+    echo "Email sent successfully";
+    }
+?>
